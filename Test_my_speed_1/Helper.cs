ff7cbc56b8e4e5439b4370f083aa55f0c8f853e7
timofey1198 <timostar98@gmail.com>
2017-09-21 22:43:57 +0300

    Добавьте файлы проекта.

---
+++ Test_my_speed_1/Helper.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_my_speed_1
+{
+    class Helper
+    {
+        /// <summary>
+        /// Функция для упращения вывода данных в консоль.
+        /// </summary>
+        /// <param name="str">
+        /// Строка, выводимая в консоль.
+        /// </param>
+        public static void Print(string str)
+        {
+            Console.WriteLine(str);
+        }
+        /// <summary>
+        /// Функция для упращения вывода данных в консоль.
+        /// </summary>
+        /// <param name="x">
+        /// Целое число, выводимое в консоль.
+        /// </param>
+        public static void Print(int x)
+        {
+            Console.WriteLine(x);
+        }
+        /// <summary>
+        /// Функция для упращения вывода данных в консоль.
+        /// </summary>
+        /// <param name="x">
+        /// Число типа double, выводимое в консоль.
+        /// </param>
+        public static void Print(double x)
+        {
+            Console.WriteLine(x);
+        }
+        /// <summary>
+        /// Функция, реализующая циклический ввод числа
+        /// </summary>
+        /// <param name="wellcome"></param>
+        /// <returns>
+        /// Введенное число
+        /// </returns>
+        public static int IntInput(string wellcome)
+        {
+            int n = 0;
+            Print(wellcome);
+            while (!int.TryParse(Console.ReadLine(), out n))
+            {
+                Print("Error. " + wellcome);
+            }
+            return n;
+        }
+        /// <summary>
+        /// Функция, реализующая циклический ввод числа
+        /// </summary>
+        /// <param name="wellcome"></param>
+        /// <returns>
+        /// Введенное число
+        /// </returns>
+        public static uint UintInput(string wellcome)
+        {
+            uint n = 0;
+            Print(wellcome);
+            while (!uint.TryParse(Console.ReadLine(), out n))
+            {
+                Print("Error. " + wellcome);
+            }
+            return n;
+        }
+        /// <summary>
+        /// Функция, реализующая циклический ввод числа
+        /// </summary>
+        /// <param name="wellcome"></param>
+        /// <returns>
+        /// Введенное число
+        /// </returns>
+        public static double DoubleInput(string wellcome)
+        {
+            double n = 0;
+            Print(wellcome);
+            while (!double.TryParse(Console.ReadLine(), out n))
+            {
+                Print("Error. " + wellcome);
+            }
+            return n;
+        }
+    }
+}
---
+++ Test_my_speed_1/Program.cs
@@ -0,0 +1,37 @@
+﻿// Дисциплина: Программирование
+// Группа: 173ПИ/2
+// Студент: Стародубцев Тимофей
+// Задача: Вариант...
+// Дата: 21.09.2017
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_my_speed_1
+{
+    class Program : Helper
+    {
+        static void Main(string[] args)
+        {
+            // Цикл повторной работы программы
+            while (true)
+            {
+                // Основной код программы
+                for (int i = 0; i < 10; i++)
+                {
+                    Print(i);
+                }
+                //
+                // Выход из цикла повторной работы
+                Print("Для продолжения нажмите любую клавишу. Для выхода нажмите Escape.");
+                if (Console.ReadKey().Key == ConsoleKey.Escape)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
