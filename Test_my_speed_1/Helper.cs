c0ef6d01d3686ba2ee1f0c6524ae1d68a25af69a
Timofey <timostar98@gmail.com>
2017-12-20 11:08:19 +0300

    Update Helper.cs

--- Test_my_speed_1/Helper.cs
+++ Test_my_speed_1/Helper.cs
@@ -8,31 +8,7 @@ namespace Test_my_speed_1
 {
     class Helper
     {
-        public delegate void SimpleFunction();
-
-        /// <summary>
-        /// Функция для упращения вывода данных в консоль.
-        /// </summary>
-        /// <param name="str">
-        /// Строка, выводимая в консоль.
-        /// </param>
-        public static void Print(string str) => Console.WriteLine(str);
-
-        /// <summary>
-        /// Функция для упращения вывода данных в консоль.
-        /// </summary>
-        /// <param name="x">
-        /// Целое число, выводимое в консоль.
-        /// </param>
-        public static void Print(int x) => Console.WriteLine(x);
-
-        /// <summary>
-        /// Функция для упращения вывода данных в консоль.
-        /// </summary>
-        /// <param name="x">
-        /// Число типа double, выводимое в консоль.
-        /// </param>
-        public static void Print(double x) => Console.WriteLine(x);
+        public static MyRandom random = new MyRandom();
 
         /// <summary>
         /// Функция, реализующая циклический ввод числа
@@ -44,10 +20,10 @@ namespace Test_my_speed_1
         public static int IntInput(string welcome)
         {
             int n = 0;
-            Print(welcome);
+            Console.Writeline(welcome);
             while (!int.TryParse(Console.ReadLine(), out n))
             {
-                Print("Error. " + welcome);
+                Console.Writeline("Error. " + welcome);
             }
             return n;
         }
@@ -56,61 +32,10 @@ namespace Test_my_speed_1
         public static int IntInput(string welcome, int minValue, int maxValue)
         {
             int n = 0;
-            Print(welcome);
+            Console.Writeline(welcome);
             while (!int.TryParse(Console.ReadLine(), out n) || n > maxValue || n < minValue)
             {
-                Print("Error. " + welcome);
-            }
-            return n;
-        }
-
-
-        public static int IntInput(string welcome, int minValue, int maxValue, SimpleFunction function)
-        {
-            int n = 0;
-            Print(welcome);
-            function();
-            while (!int.TryParse(Console.ReadLine(), out n) || n > maxValue || n < minValue)
-            {
-                Console.Clear();
-                Print("Error. " + welcome);
-                function();
-            }
-            return n;
-        }
-
-        /// <summary>
-        /// Функция, реализующая циклический ввод числа
-        /// </summary>
-        /// <param name="welcome"></param>
-        /// <returns>
-        /// Введенное число
-        /// </returns>
-        public static uint UintInput(string welcome)
-        {
-            uint n = 0;
-            Print(welcome);
-            while (!uint.TryParse(Console.ReadLine(), out n))
-            {
-                Console.Clear();
-                Print(welcome);
-            }
-            return n;
-        }
-        /// <summary>
-        /// Функция, реализующая циклический ввод числа
-        /// </summary>
-        /// <param name="welcome"></param>
-        /// <returns>
-        /// Введенное число
-        /// </returns>
-        public static double DoubleInput(string welcome)
-        {
-            double n = 0;
-            Print(welcome);
-            while (!double.TryParse(Console.ReadLine(), out n))
-            {
-                Print("Error. " + welcome);
+                Console.Writeline("Error. " + welcome);
             }
             return n;
         }
